8ff59bd4907ffac5442636018b739f97fe0526d2
[dyad] <Theivsightcompany@gmail.com>
2026-01-08 10:30:17 -0500

    [dyad] Corrected relative import paths in src/pages/Home.tsx. - wrote 1 file(s)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import Hero from '../components/Hero';
-import ProblemSolution from '../components/ProblemSolution';
-import JetOptimizer from '../components/JetOptimizer';
-import JetViz from '../components/JetViz';
-import Services from '../components/Services';
-import TrustAuthority from '../components/TrustAuthority';
-import Stats from '../components/Stats';
-import Process from '../components/ProcessPage';
-import Contact from '../components/Contact';
+import Hero from '../../components/Hero';
+import ProblemSolution from '../../components/ProblemSolution';
+import JetOptimizer from '../../components/JetOptimizer';
+import JetViz from '../../components/JetViz';
+import Services from '../../components/Services';
+import TrustAuthority from '../../components/TrustAuthority';
+import Stats from '../../components/Stats';
+import Process from '../../components/ProcessPage';
+import Contact from '../../components/Contact';
 
 const Home: React.FC = () => (
   <main>
