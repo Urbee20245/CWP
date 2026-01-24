1f6fc289ef64921eedf17ebf7983088340ffe67b
[dyad] <Theivsightcompany@gmail.com>
2026-01-23 19:55:37 -0500

    [dyad] Fixing Supabase function module import error - wrote 1 file(s)

--- supabase/functions/provision-voice-number/index.ts
+++ supabase/functions/provision-voice-number/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
-import { Retell } from 'https://esm.sh/retell-sdk@0.1.1?target=deno';
+import { Retell } from 'https://esm.sh/retell-sdk@0.1.1';
 import { handleCors, jsonResponse, errorResponse } from '../_shared/utils.ts';
 import { decryptSecret } from '../_shared/encryption.ts';
 
